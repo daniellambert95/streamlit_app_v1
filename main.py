49e2a3852aae736db9cd84385cfb0d5d714fc877
temporaryadmin <danjlambert95@gmail.com>
2021-12-17 14:19:56 +0100

    adding main.py app

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import streamlit as st
+
+st.title('Welcome to my first streamlit app')
+st.subheader('Introducing some of the capabilities of Streamlit')
+st.write('''This is my first web app.
+Enjoy it!
+''')
+
